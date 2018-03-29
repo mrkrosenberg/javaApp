be9947a5201032b08580f396c555430966117dc1
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-03-29 14:39:34 -0600

    messing around with variables

--- src/test.java
+++ src/test.java
@@ -2,10 +2,23 @@ class test
 {
     public static void main(String[] args)
         {
-            String word = "Hello Java!";
+            String word = "We";
             System.out.println(word);
             
-            char word2 = 'T';
+            char word2 = 'A';
+            char word3 = 'R';
+            char word4 = 'E';
             System.out.println(word2);
+            System.out.println(word3);
+            System.out.println(word4);
+            
+            int num = 138;
+            System.out.println(num + " in the eyes of tiger");
+            
+            float decimal = 3.14159f;
+            System.out.println("Mmmmmmm pi: " + decimal);
+            
+            boolean result = true;
+            System.out.println("This is a " + result + " statement");
     }
 }
