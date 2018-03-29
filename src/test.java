f30af7f83ad0b97b08c2aba31f0fb91c6b5715e1
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-03-29 14:59:26 -0600

    adds scanner/user input

--- src/test.java
+++ src/test.java
@@ -1,25 +1,28 @@
+
+import java.util.Scanner;
+
 class test
 {
     public static void main(String[] args)
         {
             String word = "We";
-            System.out.println(word);
+            System.out.print(word);
             
             char word2 = 'A';
             char word3 = 'R';
             char word4 = 'E';
-            System.out.println(word2);
-            System.out.println(word3);
-            System.out.println(word4);
+            System.out.print(" " + word2);
+            System.out.print(word3);
+            System.out.print(word4);
             
             int num = 138;
-            System.out.println(num + " in the eyes of tiger");
+            System.out.println(" " + num);
             
             float decimal = 3.14159f;
             System.out.println("Mmmmmmm pi: " + decimal);
             
             boolean result = true;
-            System.out.println("This is a " + result + " statement");
+            System.out.println("This is a " + result + " statement. That is pi.");
             
             byte g = 127;
             System.out.println(g);
@@ -32,5 +35,15 @@ class test
             
             double decimalLong = 5.11111111111111111111111111;
             System.out.println(decimalLong);
+            
+            System.out.println("This one will print out with a line break");
+            
+              // initializes scanner
+            Scanner input = new Scanner(System.in);
+
+            System.out.println("What's Yo' Name? ");
+            String name = input.nextLine();
+            
+            System.out.println("My name is " + name);
     }
 }
